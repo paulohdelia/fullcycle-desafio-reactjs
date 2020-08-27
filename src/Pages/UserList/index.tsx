5405440186810fb16952f126aa1b6e136ce457a5
paulohdelia <paulohdelia@gmail.com>
2020-08-27 17:46:33 -0300

    add loading animation

--- src/Pages/UserList/index.tsx
+++ src/Pages/UserList/index.tsx
@@ -1,24 +1,46 @@
 import React, { useState, useEffect } from 'react';
-
 import api from 'axios';
+
 import DisplayUser, { User } from '../../components/DisplayUser'
 import Button from '../../components/Button';
+
+import Lottie from 'react-lottie';
+import animationData from '../../lotties/loading_animation.json';
 
 const UserList: React.FC = () => {
 
     const [users, setUsers] = useState<User[]>([]);
 
     useEffect(() => {
-        api.get('https://reqres.in/api/users')
-            .then(response => setUsers(response.data.data))
-            .catch(err => console.error(err))
+        // I just want to show the cool animation
+        setTimeout(() => {
+            api.get('https://reqres.in/api/users')
+                .then(response => setUsers(response.data.data))
+                .catch(err => console.error(err))
+        }, 2000)
     }, []);
+
+    const lottieOptions = {
+        loop: true,
+        autoplay: true,
+        animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    };
 
     return (
         <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <ul>
                 {users.length === 0
-                    ? (<p>Loading...</p>)
+                    ?
+                    (<div>
+                        <Lottie
+                            options={lottieOptions}
+                            height={50}
+                            width={50}
+                        />
+                    </div>)
                     :
                     (users.map((user: User) =>
                         <DisplayUser
@@ -31,7 +53,10 @@ const UserList: React.FC = () => {
                     ))
                 }
             </ul>
-            <Button linkTo="/" fontSize="32px" title="Home" marginBottom="16px" />
+            {users.length !== 0 && (
+                <Button linkTo="/" fontSize="32px" title="Home" marginBottom="16px" />
+            )}
+
         </main>
     );
 }
